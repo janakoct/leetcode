04255d7e8367ef252ffd3b29b583ec306050ae53
Janak Mehta <Janak Mehta@10.250.59.180>
2017-06-27 18:29:01 -0700

    Palindrome number

---
+++ LeetCode/src/leetcode/easyproblems/PalindromeNumber.java
@@ -0,0 +1,19 @@
+package leetcode.easyproblems;
+
+public class PalindromeNumber {
+
+	public boolean isPalindrome(int x) {
+		int reverse = 0;
+		boolean isPalindrome = false;
+		if (!(x < 0 || x % 10 == 0)) {
+			while (x > reverse) {
+				reverse = (reverse * 10) + (x % 10);
+				x /= 10;
+			}
+		}
+		if (x == reverse || (x == reverse/10)) {
+			isPalindrome = true;
+		}
+		return isPalindrome;
+	}
+}
