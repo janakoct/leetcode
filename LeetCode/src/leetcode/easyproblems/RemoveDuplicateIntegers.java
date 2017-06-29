ab501b3db3ce8d512d5a3e21cd58dd5952033ac9
Janak Mehta <Janak Mehta@10.250.59.180>
2017-06-28 17:53:16 -0700

    Adjusted indents

--- LeetCode/src/leetcode/easyproblems/RemoveDuplicateIntegers.java
+++ LeetCode/src/leetcode/easyproblems/RemoveDuplicateIntegers.java
@@ -13,17 +13,17 @@ public class RemoveDuplicateIntegers {
 		if (nums.length == 0) {
 			return 0;
 		} else {
-		for (int i = 1; i < nums.length; ++i) {
-			if (nums[i] != nums[i-1]) {
-				nums[j++] = nums[i];
+			for (int i = 1; i < nums.length; ++i) {
+				if (nums[i] != nums[i - 1]) {
+					nums[j++] = nums[i];
+				}
 			}
-		}
 		}
 		return j;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(new RemoveDuplicateIntegers().removeDuplicates(new int[] {1,1,1,2,2,3,4,5}));
+		System.out.println(new RemoveDuplicateIntegers().removeDuplicates(new int[] { 1, 1, 1, 2, 2, 3, 4, 5 }));
 	}
 
 }
