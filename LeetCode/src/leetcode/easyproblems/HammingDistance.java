171b38d228129e1007f7fec9f482de6c54f00ba5
Janak Mehta <Janak Mehta@10.0.0.34>
2017-07-09 19:06:32 -0700

    Hamming Distance

---
+++ LeetCode/src/leetcode/easyproblems/HammingDistance.java
@@ -0,0 +1,32 @@
+package leetcode.easyproblems;
+
+/**
+ * @author Janak Mehta
+ * @link : https://leetcode.com/submissions/detail/109018829/
+ */
+public class HammingDistance {
+
+	public int hammingDistance(int x, int y) {
+		int exor = x ^ y;
+		int oneCount = Integer.bitCount(exor);
+        return oneCount;
+    }
+	
+	//this is an alternative solution where we use ANDing continuously till
+	//a zero is encountered and count the number of times we can do ANDing
+	//before facing a zero answer. The count is the number of 1s
+	public int hammingDistanceWithAnd(int x, int y) {
+		int exor = x ^ y;
+		int oneCount = 0;
+		for (; exor > 0; oneCount++) {
+			exor &= exor - 1;
+		}
+        return oneCount;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
