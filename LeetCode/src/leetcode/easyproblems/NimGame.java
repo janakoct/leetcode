8d34c3aed691f1c4f2056c080a1e02ac0d1ac781
Janak Mehta <Janak Mehta@10.0.0.34>
2017-07-13 15:09:54 -0700

    Nim Game

---
+++ LeetCode/src/leetcode/easyproblems/NimGame.java
@@ -0,0 +1,13 @@
+package leetcode.easyproblems;
+
+/**
+ * 
+ * @author Janak Mehta
+ * @link : https://leetcode.com/problems/nim-game/#/description
+ */
+public class NimGame {
+
+	public boolean canWinNim(int n) {
+        return (n%4 != 0);
+    }
+}
